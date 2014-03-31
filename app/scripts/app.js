7c30ff9f92ac9965fd16ee35079cf064bd79ac34
Bernard Doherty <bernard.doherty@icloud.com>
2014-03-31 15:10:04 -0400

    Add service to communicate with FourSquare. Display results on location and category. Add click event to launch modal of venue photos

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,8 +4,10 @@ var app = angular.module('angularFoursquareApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
+  'ui.bootstrap',
   'ngRoute'
 ]);
+
 
 app.config(function ($routeProvider) {
 	$routeProvider
